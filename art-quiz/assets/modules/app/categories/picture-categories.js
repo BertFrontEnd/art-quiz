c05070134b7d3685d11ebfbbe27f7a932e3b8a38
BertFrontEnd <48886417+BertFrontEnd@users.noreply.github.com>
2021-11-18 10:00:28 +0300

    renderPictureCategories function

--- art-quiz/assets/modules/app/categories/picture-categories.js
+++ art-quiz/assets/modules/app/categories/picture-categories.js
@@ -1,38 +1,20 @@
+import { Categories } from './categories.js';
+
 const renderPictureCategories = () => {
   const categoriesContainer = document.querySelector('#app-picture-page');
+  const configCategory = {
+    figureElementClass: 'picture-container-quiz',
+    aElementClass: 'picture-link-quiz',
+    imgElementClass: 'picture-image-quiz',
+    figcaptionElementClass: 'picture-description-quiz',
+    spanCategoryElementClass: 'picture-category-quiz',
+    spanScoreElementClass: 'picture-score-quiz',
+    step: 120,
+  };
 
   for (let i = 1; i <= 12; i++) {
-    const figureElement = document.createElement('figure');
-    figureElement.classList.add('picture-container-quiz');
-
-    const aElement = document.createElement('a');
-    aElement.classList.add('picture-link-quiz');
-    aElement.setAttribute('href', '#');
-
-    const imgElement = document.createElement('img');
-    imgElement.classList.add('picture-image-quiz');
-    imgElement.src = `https://raw.githubusercontent.com/BertFrontEnd/art-quiz-images/master/art-quiz-images/img/${
-      120 + i + 10
-    }.jpg`;
-    imgElement.alt = `Image ${(i = i < 10 ? '0' + i : i)}`;
-
-    const figcaptionElement = document.createElement('figcaption');
-    figcaptionElement.classList.add('picture-description-quiz');
-
-    const spanCategoryElement = document.createElement('span');
-    spanCategoryElement.classList.add('picture-category-quiz');
-    spanCategoryElement.textContent = `${i}`;
-
-    const spanScoreElement = document.createElement('span');
-    spanScoreElement.classList.add('picture-score-quiz');
-    spanScoreElement.textContent = `0/10`;
-
-    categoriesContainer.append(figureElement);
-    figureElement.append(aElement);
-    aElement.append(imgElement);
-    figureElement.append(figcaptionElement);
-    figcaptionElement.append(spanCategoryElement);
-    figcaptionElement.append(spanScoreElement);
+    const category = new Categories(categoriesContainer, configCategory, i);
+    category.renderCategory();
   }
 };
 
